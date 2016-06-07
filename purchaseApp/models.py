458e48f8a3e01af2867fbe106c0397ef2c5f9b63
S Naghdi <s.naghdi68@gmail.com>
2016-06-07 17:36:17 +0430

    add __str__ function

--- purchaseApp/models.py
+++ purchaseApp/models.py
@@ -34,3 +34,5 @@ class Portion (models.Model):
     status = models.BooleanField(default=False)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(default=timezone.now)
+    def __str__(self):
+        return self.purchase.title
