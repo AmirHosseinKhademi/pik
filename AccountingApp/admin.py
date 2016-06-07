f92e0e91ec638d214438828b84244179438e5c59
S Naghdi <s.naghdi68@gmail.com>
2016-06-07 17:30:22 +0430

    add accounting table to admin interface

--- AccountingApp/admin.py
+++ AccountingApp/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
-
+from UserApp.admin import admin_site
+from .models import AccountingModel
 # Register your models here.
+
+class AccountingAdmin(admin.ModelAdmin):
+    fields = ('amount', 'user', 'action', 'portion')
+    list_display = ( 'user_name', 'amount', 'action', 'portion', 'date')
+    list_display_links = ('user_name', )
+    list_filter = ('user', )
+    ordering = ('user', )
+    preserve_filters = False
+
+    def user_name(self, obj):
+        return obj.user.email
+
+    user_name.short_description = 'ایمیل کاربر'
+
+    def get_portion(self, obj):
+        if obj.portion:
+            return obj.portion.purchase.title
+        else:
+            return None
+
+    get_portion.short_description = 'نام خرید مربوط به سهم'
+
+admin_site.register(AccountingModel, AccountingAdmin)
--- AccountingApp/models.py
+++ AccountingApp/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 from UserApp.models import CustomizedUser
 from purchaseApp.models import Portion
+from django.utils.translation import ugettext_lazy as _
 
 class AccountingModel(models.Model):
-    amount = models.PositiveIntegerField()
-    user = models.ForeignKey(CustomizedUser, on_delete=models.CASCADE)
-    portion = models.ForeignKey(Portion, blank=True,default=0, null=True)
-    action = models.IntegerField()
-    date = models.DateTimeField(auto_now_add=True)
+    amount = models.PositiveIntegerField(verbose_name='مقدار')
+    user = models.ForeignKey(CustomizedUser, on_delete=models.CASCADE, verbose_name='کاربر')
+    portion = models.ForeignKey(Portion, blank=True,default=0, null=True, verbose_name='سهم')
+    action = models.IntegerField(verbose_name='فعالیت')
+    date = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ')
+    class Meta:
+        verbose_name = (_("مدیریت تراکنش‌ها"))
+        verbose_name_plural = (_("مدیریت تراکنش‌ها"))
     # 0 = Pardakhte Pik
     # 1 = Sharzhe Hesab
     # 2 = Darkhaste Tasviye
