364701ac972db738a06d12570932ce1443576b44
S Naghdi <s.naghdi68@gmail.com>
2016-06-08 15:52:57 +0430

    fields name in admin site in farsi

--- groupApp/models.py
+++ groupApp/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
 from UserApp.models import CustomizedUser
+from django.utils.translation import ugettext_lazy as _
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=255)
-    admin = models.ForeignKey(CustomizedUser, related_name='group_admin')
-    member = models.ManyToManyField(CustomizedUser, blank=True, related_name='group_member')
-    creation_datetime = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255, verbose_name='عنوان گروه')
+    admin = models.ForeignKey(CustomizedUser, related_name='group_admin', verbose_name='مدیر گروه ')
+    member = models.ManyToManyField(CustomizedUser, blank=True, related_name='group_member', verbose_name='اعضای گروه')
+    creation_datetime = models.DateTimeField(auto_now_add=True, verbose_name='زمان ایجاد گروه')
 
     def __str__(self):
         return self.title
@@ -16,6 +17,9 @@ class Group(models.Model):
         self.admin = user_id
         self.save()
 
+    class Meta:
+        verbose_name = (_("مدیریت گروه‌ها"))
+        verbose_name_plural = (_("مدیریت گروه‌ها"))
 
 # class UserGroup(models.Model):
 #     group = models.ForeignKey(Group)
