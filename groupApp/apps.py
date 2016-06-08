82e8c32c790c380436c1da2f2c032d46069e3dc8
S Naghdi <s.naghdi68@gmail.com>
2016-06-08 15:53:30 +0430

    use this conf for group app

--- groupApp/apps.py
+++ groupApp/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class GroupConfig(AppConfig):
     name = 'groupApp'
+    verbose_name = (_("مدیریت گروه‌ها"))
